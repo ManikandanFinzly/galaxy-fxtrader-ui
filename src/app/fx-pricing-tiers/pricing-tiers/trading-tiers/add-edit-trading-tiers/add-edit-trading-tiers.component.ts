dd8c1d6993b576b6fb684c22320a0594adae1d73
Dheepa Kanagaraj <dheepa.kanagaraj@finzly.com>
2024-05-29 12:24:45 +0530

    Edit screen Changes for Trading Tier UI

--- src/app/fx-pricing-tiers/pricing-tiers/trading-tiers/add-edit-trading-tiers/add-edit-trading-tiers.component.ts
+++ src/app/fx-pricing-tiers/pricing-tiers/trading-tiers/add-edit-trading-tiers/add-edit-trading-tiers.component.ts
@@ -17,7 +17,7 @@ export class AddEditTradingTiersComponent implements OnInit {
 
   tradingTierForm: FormGroup;
   tradingTierId;
-  from: string[] = ['00:00', '00:15', '00:30', '00:45', '01:00', '01:15', '01:30', '01:45', '02:00', '02:15', '02:30', '02:45',
+  fromTime : string[] = ['00:00', '00:15', '00:30', '00:45', '01:00', '01:15', '01:30', '01:45', '02:00', '02:15', '02:30', '02:45',
     '03:00', '03:15', '03:30', '03:45', '04:00', '04:15', '04:30', '04:45', '05:00', '05:15', '05:30', '05:45',
     '06:00', '06:15', '06:30', '06:45', '07:00', '07:15', '07:30', '07:45', '08:00', '08:15', '08:30', '08:45',
     '09:00', '09:15', '09:30', '09:45', '10:00', '10:15', '10:30', '10:45', '11:00', '11:15', '11:30', '11:45',
@@ -25,7 +25,7 @@ export class AddEditTradingTiersComponent implements OnInit {
     '15:00', '15:15', '15:30', '15:45', '16:00', '16:15', '16:30', '16:45', '17:00', '17:15', '17:30', '17:45',
     '18:00', '18:15', '18:30', '18:45', '19:00', '19:15', '19:30', '19:45', '20:00', '20:15', '20:30', '20:45',
     '21:00', '21:15', '21:30', '21:45', '22:00', '22:15', '22:30', '22:45', '23:00'];
-  to: string[] = ['00:00', '00:15', '00:30', '00:45', '01:00', '01:15', '01:30', '01:45', '02:00', '02:15', '02:30', '02:45',
+  toTime : string[] = ['00:00', '00:15', '00:30', '00:45', '01:00', '01:15', '01:30', '01:45', '02:00', '02:15', '02:30', '02:45',
     '03:00', '03:15', '03:30', '03:45', '04:00', '04:15', '04:30', '04:45', '05:00', '05:15', '05:30', '05:45',
     '06:00', '06:15', '06:30', '06:45', '07:00', '07:15', '07:30', '07:45', '08:00', '08:15', '08:30', '08:45',
     '09:00', '09:15', '09:30', '09:45', '10:00', '10:15', '10:30', '10:45', '11:00', '11:15', '11:30', '11:45',
@@ -33,7 +33,7 @@ export class AddEditTradingTiersComponent implements OnInit {
     '15:00', '15:15', '15:30', '15:45', '16:00', '16:15', '16:30', '16:45', '17:00', '17:15', '17:30', '17:45',
     '18:00', '18:15', '18:30', '18:45', '19:00', '19:15', '19:30', '19:45', '20:00', '20:15', '20:30', '20:45',
     '21:00', '21:15', '21:30', '21:45', '22:00', '22:15', '22:30', '22:45', '23:00'];
-  defaultPrices = ['Flat 2%', 'Flat 3%'];
+  defaultPrices = ['Flat 1%', 'Flat 3%'];
   rateSources: string[] = ['Online', 'Offline'];
   ccyGroupId;
   isDefaultTradingTier = false;
@@ -66,6 +66,7 @@ export class AddEditTradingTiersComponent implements OnInit {
       this.tradingTierId = data.tradingTierId;
       if (data.ccyGroupId) {
         this.ccyGroupId = data.ccyGroupId;
+        this.isToggled = data.isAllDay;
       }
     }
   }
@@ -135,7 +136,12 @@ export class AddEditTradingTiersComponent implements OnInit {
         (data) => {
           if (data) {
             this.tradingTierForm.patchValue({
-              tierName: data.tierName,
+              tierName : data.tierName,
+              isToggled: data.isAllDay,
+              fromTime : data.fromTime,
+              toTime : data.toTime,
+              rateSource : data.rateSource,
+              noQuoteMessage : data.noQuoteMsg
             });
 
             if (this.ccyGroupId) {
