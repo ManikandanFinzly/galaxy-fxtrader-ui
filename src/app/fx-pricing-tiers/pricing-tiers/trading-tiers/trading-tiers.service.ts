40b2bb72d8cb88733283cb496bc5dcd046377a7b
Keerthana Devi <keerthanadevi@finzly.com>
2024-06-04 10:33:34 +0530

    Trading tier get api integration.

--- src/app/fx-pricing-tiers/pricing-tiers/trading-tiers/trading-tiers.service.ts
+++ src/app/fx-pricing-tiers/pricing-tiers/trading-tiers/trading-tiers.service.ts
@@ -1,13 +1,16 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Globals } from 'globals.service';
-import { Observable } from 'rxjs';
+import { Observable, throwError,  } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TradingTiersService {
+
+  apiUrl = '/fxtrader/tradingtier';
 
   constructor(private http: HttpClient, private global: Globals) { }
 
@@ -17,13 +20,30 @@ export class TradingTiersService {
     return this.http.get<any[]>(this.jsonUrl);
   }
 
-  public getTradingTierById(id: any): Observable<any | undefined> {
-    return this.getTradingTier().pipe(
-      map(items => items.find(item => item.id === id))
-    );
-  }
-
   public getDefaultPriceNameById(id) {
     return 'Flat 1%';
   }
+
+  public getAllTradingTier(isParent:boolean){
+    return (this.http.get(`${this.apiUrl}?is-parent=${isParent}`));
+  }
+
+  public getTradingTierById(id: string){
+    return (this.http.get(`${this.apiUrl}/${id}`));
+  }
+
+  deleteTradingTier(id: string, tierItemId: string): Observable<any> {
+    const url = `${this.apiUrl}/${id}`;
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    const params = new HttpParams().set('tier-item-id', tierItemId);
+    return this.http.delete(url, { headers, params }).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(error: any): Observable<never> {
+    console.error('An error occurred:', error);
+    return throwError('Something bad happened; please try again later.');
+  }
+
 }
